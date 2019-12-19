9c3962d0cdd509a90ec8de7b2fd4cb23b02bf856
Yanan Lyu <Yanan.lyu@gmail.com>
2019-12-19 18:37:39 -0500

    add mod+Shift+m for change monitor to original

--- xrandrexternal4k.sh
+++ xrandrexternal4k.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output HDMI1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output DP1 --off --output DP2 --off
---
+++ xrandroriginal.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --auto --output HDMI1 --off --pos 0x0 --rotate normal --output HDMI2 --off --output DP1 --off --output DP2 --off
