8de1e86beb3904b06d87da88eb2ed82a90a43b06
lyu <lyu@pop-os.localdomain>
2020-06-07 13:12:00 -0400

    add blur pictures

--- blurlock.sh
+++ blurlock.sh
@@ -3,13 +3,17 @@
 read res<<<$(xrandr |grep \* |awk '{print $1}')
 #convert /home/lyu/Pictures/Wallpapers/mars.png -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/girlgreat.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
+convert /home/lyu/Pictures/Wallpapers/rsz_2ciyuan.jpg -resize $res -blur 0x3 /tmp/screenshotblur.png
+#convert /home/lyu/Pictures/Wallpapers/rsz_whiletrue.png -resize $res -blur 0x5 /tmp/screenshotblur.png
+#convert /home/lyu/Pictures/Wallpapers/rsz_thinkbetter.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/shuimo.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/mountain.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/mac.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/linuxkernel.png -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/codinglife.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
-convert /home/lyu/Pictures/Wallpapers/me.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
+#convert /home/lyu/Pictures/Wallpapers/me.jpg -resize $res -blur 0x5 /tmp/screenshotblur.png
 #convert /home/lyu/Pictures/Wallpapers/codes.png -resize $res -blur 0x5 /tmp/screenshotblur.png
+
 #rm /tmp/screenshot.png
 i3lock -i /tmp/screenshotblur.png
 exit 0
